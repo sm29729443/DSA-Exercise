ddf97a154d0740812025ed0ea4a3a1e244245685
Fish <sm29729443@gmail.com>
2024-10-21 13:20:39 +0800

    initial project

---
+++ src/test/java/TestBench.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * ClassName: TestBench
+ * Package: PACKAGE_NAME
+ */
+public class TestBench {
+
+    @Test
+    public void test1(){
+        System.out.println("initial");
+    }
+}
